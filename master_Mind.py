ed2942768c432523fb23821456bc00cd8bf7bc20
Kane026 <frentzkane@gmail.com>
2025-06-04 13:24:26 +0200

    Cheat opgelost

--- master_Mind.py
+++ master_Mind.py
@@ -35,6 +35,7 @@ def play_Mastermind():
     print("Guess the 4-digit code. Each digit is from 1 to 6. You have 10 attempts.")
     secret_Code = generate_Code()
     attempts = 10
+    password = "admin"
 
     for attempt in range(1, attempts + 1):
         guess = ""
@@ -44,7 +45,11 @@ def play_Mastermind():
             valid_Guess = len(guess) == 4 and all(c in "123456" for c in guess)
             if not valid_Guess:
                 print("Invalid input. Enter 4 digits, each from 1 to 6.")
-            show_Secret(secret_Code) if guess == "cheat" else False
+            if guess == "cheat": passwordinput = input("Input password") 
+            if passwordinput == password:
+                show_Secret(secret_Code)  
+            else: print("incorrect password")
+            continue
 
         black, white = get_Feedback(secret_Code, guess)
         print(f"Black pegs (correct position): {black}, White pegs (wrong position): {white}")
